1185a50694dfe5961eb822736fa22398260f4c69
Quang Sanh <buiquangsanh1501@gmail.com>
2025-07-05 09:40:01 +0700

    test cicd

--- server.js
+++ server.js
@@ -25,11 +25,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/polls", pollRoutes);
 app.use("/api/votes", voteRoutes);
 
-//test cicd
-
-app.get("/hello", (req, res) => {
+//test ci_cd
+app.get("/hello", (req,res){
   res.send("Hello World");
-});
+})
 
 const PORT = process.env.PORT || 3000;
 
